88f0b2b43164cb66cd209e0b87b7edc4885bccf9
zareena <sk.zareena25@gmail.com>
2025-06-05 13:54:07 +0530

    text and animation added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,19 @@
-import logo from "../assets/Images/Alfredlogo.png";
+import logo from "../assets/Images/PathsetterLogo.png";
 
 const Header = () => {
- 
   return (
     <header>
       <div className="flex flex-row items-center justify-between pt-10 px-32">
         <div className="flex items-center gap-16">
           <img src={logo} alt="Logo" />
-          <div className="flex gap-16">
-            <a href="#hero" >Home</a>
-            <a href="#aboutus" >About Us</a>
-            <a href="#features" >Features</a>
+          <div className="flex gap-16 text-white">
+            <a href="#hero">HOME</a>
+            <a href="#features">FEATURES</a>
+            <a href="#aboutus">ABOUT US</a>
           </div>
         </div>
-        <h4 className="text-black rounded-full border-[2px] p-1 px-4 text-lg">
-          Get a Demo
+        <h4 className="rounded-full border-[2px] p-1  px-4 text-lg text-white">
+          GET A DEMO
         </h4>
       </div>
     </header>
--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React from "react";
 import timeline from "../assets/Images/TimeLine.jpg";
 
 const Aboutus = () => {
   return (
-     <div id="aboutus" className="w-full bg-black px-28 py-20 scroll-mt-28">
-      
-        <div className="flex flex-row w-full items-center justify-center space-x-10">
-          <div className="w-3/5 text-white">
-            <p className="uppercase text-xl">Introducing <span className="font-semibold">Alfred</span></p>
-            <h1 className="text-3xl font-bold my-2">The AI Project Decisions Engine</h1>
-            <p className="text-lg font-sans ">
-              An AI-native project management platform built to drive speed, clarity, and certainty across your infrastructure lifecycle.
-            </p>
-          </div>
-          <div>
-            <img src={timeline} alt="Time Line" className="w-full max-w-4xl" />
-          </div>
+    <div id="aboutus" className="w-full bg-black px-28 py-20 scroll-mt-28">
+      <div className="flex flex-row w-full items-center justify-center space-x-10">
+        <div className="w-3/5 text-white">
+          <p className="uppercase text-xl">About us</p>
+          <h1 className="text-3xl font-bold my-2">
+            Solving Infrastructure's Toughest Problems with AI. Together.
+          </h1>
+          <p className="text-lg font-sans ">
+            At Pathsetter AI, we're building the future project management -
+            powered by intelligence, simplicity, and deep industry empathy.
+          </p>
+          <p>
+            Our founding team has lived the pain of infrastructure execution
+            first-hand. We've seen how projects falter under complexity,
+            communication gaps, and fragmented tools. Th
+          </p>
+        </div>
+        <div>
+          {/* <img src={timeline} alt="Time Line" className="w-full max-w-4xl" /> */}
         </div>
       </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Aboutus
+export default Aboutus;
