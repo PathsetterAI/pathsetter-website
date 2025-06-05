1f6ea0af54ea9401f2c5e067002d0a739832a01f
zareena <sk.zareena25@gmail.com>
2025-06-05 17:13:24 +0530

    Enterprise ready svgs

---
+++ src/Components/svg/EnterpriseSvg/RoleAccessControl.jsx
@@ -0,0 +1,33 @@
+const RoleAccessControl = () => (
+  <svg
+    width="40"
+    height="40"
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M31.6666 17.5002V16.6667C31.6666 10.3814 31.6664 7.23868 29.7139 5.28606C27.7612 3.3335 24.6186 3.3335 18.3332 3.3335C12.0481 3.3335 8.90533 3.3336 6.95272 5.28615C5.00015 7.23875 5.00012 10.3814 5.00007 16.6666L5 24.1668C4.99995 29.6458 4.99993 32.3855 6.51313 34.2293C6.7902 34.567 7.09977 34.8765 7.43738 35.1537C9.28128 36.6668 12.0208 36.6668 17.4999 36.6668"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M11.6665 11.6665H24.9998M11.6665 18.3332H18.3332"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M30 30.8332L27.5 29.9165V25.8332M20 29.1665C20 33.3087 23.3578 36.6665 27.5 36.6665C31.6422 36.6665 35 33.3087 35 29.1665C35 25.0243 31.6422 21.6665 27.5 21.6665C23.3578 21.6665 20 25.0243 20 29.1665Z"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+
+export default RoleAccessControl;
